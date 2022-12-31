3293caaed2212299cc59e7e407ad19e64dd1cc23
Aizistral <admin@aizistral.com>
2022-12-31 17:09:20 +0100

    Complete the project

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const multer = require("multer");
 
-var app = express();
+require("dotenv").config();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use("/api/fileanalyse", multer({ storage: multer.memoryStorage() }).single("upfile"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => res.sendFile(process.cwd() + "/views/index.html"));
+
+app.post("/api/fileanalyse", (req, res) => {
+	if (!req.file) return res.status(400).json({ error: "No uploaded file provided." });
+
+	return res.json({
+		name: req.file.originalname,
+		type: req.file.mimetype,
+		size: req.file.size
+	});
 });
 
-
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+app.listen(process.env.PORT, () => console.log("IT'S ALIVE on port " + process.env.PORT));
